e8b35f7b6a51dd729441e5ede4e6c06353170910
Michal Danilak <mimino.coder@gmail.com>
2013-11-21 21:31:06 +0100

    Created basic cmdline interface.

---
+++ bin/marathoner.bat
@@ -0,0 +1,4 @@
+@echo off
+rem Windows command-line tool for Marathoner
+
+python -mmarathoner.cmdline %*
---
+++ marathoner/cmdline.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def execute(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+
+if __name__ == '__main__':
+    execute()
