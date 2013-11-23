f21c6bd97f1cf8d4f743eed21af2490103dce41a
Michal Danilak <mimino.coder@gmail.com>
2013-11-23 20:43:37 +0100

    Added AsyncReader class.

---
+++ marathoner/async_reader.py
@@ -0,0 +1,21 @@
+import threading
+
+
+class AsyncReader(threading.Thread):
+    '''Asynchronously read lines from the stream and call the callback for
+    each line.
+    '''
+    def __init__(self, stream, cb):
+        super(AsyncReader, self).__init__()
+        self.stream = stream
+        self.cb = cb
+
+    def run(self):
+        while True:
+            try:
+                line = self.stream.readline()
+            except:
+                break
+            if not line:
+                break
+            self.cb(line)
