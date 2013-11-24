649f7b32e54cf5f245cfb93e297720bacf7547c5
Michal Danilak <mimino.coder@gmail.com>
2013-11-24 01:46:30 +0100

    Added `multi test` command.

---
+++ marathoner/commands/multi_test.py
@@ -0,0 +1,33 @@
+import re
+
+from marathoner.commands.base import BaseCommand
+
+
+class Command(BaseCommand):
+    syntax = '<seed1> <seed2> [vis params]'
+    help = 'run batch of tests from interval [seed1, seed2]'
+
+    cmd_re = re.compile(r'^\s*(\d+)\s+(\d+)\s*([^\d\s].*)?\s*$')
+    def is_match(self, command):
+        return self.cmd_re.match(command)
+
+    def handle(self, command):
+        match = self.cmd_re.match(command)
+        seed1 = int(match.group(1))
+        seed2 = int(match.group(2))
+        vis_params = match.group(3) or ''
+
+        if seed2 < seed1:
+            print 'Error: seed1 can\'t be larger than seed2!'
+            return
+
+        self.contest.multiple_tests_starting(seed2-seed1+1)
+        for seed in xrange(seed1, seed2+1):
+            self.contest.one_test_starting(seed)
+            visualizer_stdout, solution_stderr = self.executor.run(seed, False, vis_params)
+            best_score = self.project.scores[seed]
+            current_score = self.contest.extract_score(seed, visualizer_stdout, solution_stderr)
+            self.project.scores[seed] = current_score
+            self.contest.one_test_ending(seed, visualizer_stdout, solution_stderr, best_score, current_score)
+        self.project.scores.save()
+        self.contest.multiple_tests_ending(seed2-seed1+1)
