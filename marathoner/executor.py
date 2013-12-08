9429ce802b64e7732e152e7002d0fcb5a738fed9
Michal Danilak <mimino.coder@gmail.com>
2013-12-08 02:52:02 +0100

    Added ability to kill solution. Refactor of executor and mediator.

--- marathoner/executor.py
+++ marathoner/executor.py
@@ -1,18 +1,22 @@
+import os
 import pickle
+import signal
 import socket
 import subprocess
 import sys
+import threading
 
 from marathoner import MARATHONER_PORT
-from marathoner.async_reader import AsyncReader
+from marathoner.utils.async_reader import AsyncReader
+from marathoner.utils.key_press import get_key_press
 
 
 class Executor(object):
     def __init__(self, project):
         self.project = project
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind(('', MARATHONER_PORT))
-        self.sock.listen(1)
+        self.sock.bind(('127.0.0.1', MARATHONER_PORT))
+        self.sock.listen(0)
 
     def __del__(self):
         self.sock.close()
@@ -31,6 +35,9 @@ class Executor(object):
         @param special_params: additional params to pass to visualizer
         @type special_params: string
         '''
+        self.is_single_test = is_single_test
+        self.solution_stderr, self.visualizer_stdout = [], []
+
         # start visualizer
         params = self.get_visualizer_params(seed, is_single_test, special_params)
         si = None
@@ -41,58 +48,54 @@ class Executor(object):
         visualizer = subprocess.Popen(
             params,
             shell=False,
+            bufsize=1,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             stdin=subprocess.PIPE,
-            cwd=None,
             universal_newlines=True,
             startupinfo=si)
 
         # accept connection from mediator
         conn, addr = self.sock.accept()
-        socket_reader = conn.makefile('r')
-        socket_writer = conn.makefile('w')
+        self.socket_reader = conn.makefile('r')
+        self.socket_writer = conn.makefile('w')
         # initialize mediator
         mediator_settings = {
             'is_single_test': is_single_test,
             'testcase': self.project.testcase,
             'solution': self.project.solution}
-        pickle.dump(mediator_settings, socket_writer)
-        socket_writer.flush()
+        pickle.dump(mediator_settings, self.socket_writer)
+        self.socket_writer.flush()
+        self.solution_pid = pickle.load(self.socket_reader)
 
-        # read standard error output of solution
-        solution_stderr = []
-        def solution_stderr_cb(line):
-            solution_stderr.append(line)
-            if is_single_test or line[0] == '!':
-                sys.stdout.write(line)
-        solution_stderr_reader = AsyncReader(socket_reader, solution_stderr_cb)
+        solution_stderr_reader = AsyncReader(self.socket_reader, self._solution_stderr_cb)
+        visualizer_stdout_reader = AsyncReader(visualizer.stdout, self._visualizer_stdout_cb)
+        visualizer_stderr_reader = AsyncReader(visualizer.stderr, self._visualizer_stderr_cb)
         solution_stderr_reader.start()
-
-        # read standard output from visualizer
-        visualizer_stdout = []
-        def visualizer_stdout_cb(line):
-            visualizer_stdout.append(line)
-        visualizer_stdout_reader = AsyncReader(visualizer.stdout, visualizer_stdout_cb)
         visualizer_stdout_reader.start()
-
-        # read standard error output from visualizer
-        def visualizer_stderr_cb(line):
-            sys.stdout.write(line)
-        visualizer_stderr_reader = AsyncReader(visualizer.stderr, visualizer_stderr_cb)
         visualizer_stderr_reader.start()
+
+        # check for kill signal from user
+        self.solution_killed = False
+        self.kill_event = threading.Event()
+        kill_solution = threading.Thread(target=get_key_press,
+                                         args=['q', self.kill_event, self._kill_solution_cb])
+        kill_solution.start()
 
         # wait for readers to finish
         solution_stderr_reader.join()
         visualizer_stdout_reader.join()
         visualizer_stderr_reader.join()
 
+        self.kill_event.set()
+        kill_solution.join()
+
         # close the resources
-        socket_reader.close()
-        socket_writer.close()
+        self.socket_reader.close()
+        self.socket_writer.close()
         conn.close()
 
-        return (visualizer_stdout, solution_stderr)
+        return (self.visualizer_stdout, self.solution_stderr)
 
     def get_visualizer_params(self, seed, is_single_test, special_params):
         exec_params = [
@@ -104,3 +107,28 @@ class Executor(object):
 
         return exec_params + self.project.params + special_params + seed_params
 
+    def _kill_solution_cb(self):
+        self.solution_killed = True
+        try:
+            os.kill(self.solution_pid, signal.SIGTERM)
+        except:
+            pass
+
+    def _solution_stderr_cb(self, line):
+        '''Read standard error output of solution and store it locally.
+        For single tests, output it real-time.
+        For multi tests, output only lines starting with "!".
+        '''
+        self.solution_stderr.append(line)
+        if self.is_single_test or line[0] == '!':
+            sys.stdout.write(line)
+
+    def _visualizer_stdout_cb(self, line):
+        '''Read standard output from visualizer and store it locally.
+        '''
+        self.visualizer_stdout.append(line)
+
+    def _visualizer_stderr_cb(self, line):
+        '''Read standard error output from visualizer and output it.
+        '''
+        sys.stdout.write(line)
--- marathoner/mediator.py
+++ marathoner/mediator.py
@@ -1,29 +1,32 @@
 '''
-Mediator is a man-in-the-middle app between the visualizer and the
-solution created by the competitor.
+Mediator is the python program that is actually executed by visualizer,
+instead of the user's solution. Mediator executes user's solution as a child
+process and redirects all the communication between the visualizer and the
+solution.
 
-Its main purpose is to redirect inputs and outputs between the visualizer and
-the solution, so that everything works as expected.
-
-Its secondary purpose is to store the input received from the visualizer to
-the file, send the standard error output from solution to Marathoner and
-listen for a kill-signal in a case when user decided to kill the
-solution (by pressing 'Q').
+On background, it communacates with Marathoner through sockets.
+This injection hack allows to:
+  - export input data from visualizer into testcase file
+  - send stderr from solution to Marathoner so it can be outputed in real-time
+  - listen for kill-signal from Marathoner to kill the solution
+    (when it gets stuck, for example)
 '''
 import pickle
+import shlex
+import signal
 import socket
 import subprocess
 import sys
 
 from marathoner import MARATHONER_PORT
-from marathoner.async_reader import AsyncReader
+from marathoner.utils.async_reader import AsyncReader
 
 
 class Mediator(object):
     def __init__(self):
         # connect to marathoner
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect(('localhost', MARATHONER_PORT))
+        self.sock.connect(('127.0.0.1', MARATHONER_PORT))
 
         self.socket_reader = self.sock.makefile('r')
         self.socket_writer = self.sock.makefile('w')
@@ -46,28 +49,44 @@ class Mediator(object):
             si.dwFlags = subprocess.STARTF_USESHOWWINDOW
             si.wShowWindow = subprocess.SW_HIDE
 
-        self.solution = subprocess.Popen(
-            self.solution,
-            shell=False,
+        self.solution_proc = subprocess.Popen(
+            shlex.split(self.solution),
+            shell=False,  # don't set to True, otherwise it is impossible to kill it
+            bufsize=1,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             stdin=subprocess.PIPE,
-            cwd=None,
             universal_newlines=True,
             startupinfo=si)
+        pickle.dump(self.solution_proc.pid, self.socket_writer)
+        self.socket_writer.flush()
 
         # setup redirections
         visualizer_input_reader = AsyncReader(sys.stdin, self._visualizer_input_cb)
         visualizer_input_reader.setDaemon(True)
-        solution_output_reader = AsyncReader(self.solution.stdout, self._solution_output_cb)
-        solution_error_reader = AsyncReader(self.solution.stderr, self._solution_error_db)
-
+        solution_output_reader = AsyncReader(self.solution_proc.stdout, self._solution_output_cb)
+        solution_error_reader = AsyncReader(self.solution_proc.stderr, self._solution_error_cb)
         visualizer_input_reader.start()
         solution_output_reader.start()
         solution_error_reader.start()
 
+        self.solution_proc.wait()
         solution_output_reader.join()
         solution_error_reader.join()
+
+        # return code from solution
+        code = self.solution_proc.poll()
+        if code:
+            signal_name = dict((k, v) for v, k in signal.__dict__.iteritems()
+                                      if v.startswith('SIG')) \
+                          .get(code, '')
+            if signal_name:
+                s = '%s (%s)' % (code, signal_name)
+            else:
+                s = '%s' % code
+            msg = 'WARNING: Your solution ended with non-zero return value: %s\n' % s
+            self.socket_writer.write(msg)
+            self.socket_writer.flush()
 
         # close the resources
         if self.testcase_file:
@@ -78,11 +97,11 @@ class Mediator(object):
 
     def _visualizer_input_cb(self, line):
         '''Read input from visualizer and redirect it to the solution.
-        If needed, write the input to the separate file.
+        Also export the input data to testcase file, if available.
         '''
-        if self.solution.poll() is None:
-            self.solution.stdin.write(line)
-            self.solution.stdin.flush()
+        if self.solution_proc.poll() is None:
+            self.solution_proc.stdin.write(line)
+            self.solution_proc.stdin.flush()
         if self.testcase_file:
             self.testcase_file.write(line)
             self.testcase_file.flush()
@@ -93,7 +112,7 @@ class Mediator(object):
         sys.stdout.write(line)
         sys.stdout.flush()
 
-    def _solution_error_db(self, line):
+    def _solution_error_cb(self, line):
         '''Read standard error output from the solution and redirect it back to
         Marathoner.
         '''
