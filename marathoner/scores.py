30803d210acf43a7ca1c7e75bb0f80cc5d31c2c1
Michal Danilak <mimino.coder@gmail.com>
2013-12-27 17:58:50 +0100

    Store run time in scores file.

--- marathoner/scores.py
+++ marathoner/scores.py
@@ -1,12 +1,13 @@
 from os import path
+import re
 
 
 class Score(object):
     '''Encapsulates score and run-time from the single test.'''
     def __init__(self, seed, score, run_time=None):
-        self.seed = seed
-        self.score = score
-        self.run_time = run_time
+        self.seed = int(seed)
+        self.score = float(score)
+        self.run_time = float(run_time or 0.0)
 
     def __str__(self):
         return '<Test-%s Score: %s Time: %s>' % (self.seed, self.score, self.time)
@@ -19,6 +20,14 @@ class Score(object):
             return score2
         if not score2.score:
             return score1
+
+        # for the same score, compare run times
+        if score1.score == score2.score:
+            if not score1.run_time:
+                return score2
+            if not score2.run_time:
+                return score1
+            return score1 if score1.run_time < score2.run_time else score2
 
         if maximize == (score1.score > score2.score):
             return score1
@@ -36,6 +45,8 @@ class Score(object):
 
 
 class Scores(object):
+    line_re = re.compile(r'^\D*(?P<seed>\d[\d.,]*)\D+(?P<score>\d[\d.,]*)(\D+(?P<time>\d[\d.,]*))?\D*$', re.IGNORECASE)
+
     def __init__(self, project, scores_file):
         self.project = project
         self.scores_file = scores_file
@@ -43,11 +54,16 @@ class Scores(object):
         # read scores from the file
         if path.exists(scores_file):
             with open(scores_file, 'r') as f:
-                for line in f:
-                    seed, score = line.split(':')
-                    seed = int(seed)
-                    score = float(score)
-                    self.best_scores[seed] = Score(seed, score)
+                for num, line in enumerate(f, 1):
+                    match = self.line_re.match(line)
+                    if match:
+                        score = Score(
+                            match.group('seed'),
+                            match.group('score'),
+                            match.group('time'))
+                        self.best_scores[score.seed] = score
+                    elif not line.isspace():
+                        raise RuntimeError('Invalid line (%s) in "%s" score file:\n\t%s' % (num, scores_file, line))
         else:
             # create empty file
             with open(scores_file, 'w') as f:
@@ -68,7 +84,7 @@ class Scores(object):
         with open(self.scores_file, 'w') as f:
             for seed in sorted(self.seeds):
                 score = self.best_scores[seed]
-                f.write('%d: %f\n' % (score.seed, score.score))
+                f.write('Seed: %d \tScore: %f \tRun time: %f\n' % (score.seed, score.score, score.run_time))
 
     def update(self, scores):
         for seed in scores.seeds:
